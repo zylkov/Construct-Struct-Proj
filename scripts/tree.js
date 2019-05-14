53f2f50d60c21034e44d28f8e644daa5cf860a1a
Pixel-Tentacles <zylkov2010@gmail.com>
2019-05-14 20:40:01 +0300

    change struct

--- scripts/tree.js
+++ scripts/tree.js
@@ -428,36 +428,6 @@ function getInfoFunct(idParent, id){
 
 
 
-function turnLoadingOnModal(modals, on){
-    if(on){
-        modals.find(".loading").removeClass("d-none").addClass("d-flex");
-        modals.find(".loaded").addClass("d-none");
-    }
-    else{
-        modals.find(".loading").removeClass("d-flex").addClass("d-none");
-        modals.find(".loaded").removeClass("d-none");
-    }
-}
-
-function promptModal(title = "Заголовок", question = "Вопрос?" , defualtAnswer = "", callbackAnswered = ()=>{}){
-    modals = $("#proptModal");
-    modals.find("#proptModalTitle").text(title);
-    modals.find("#questionText").text(question);
-    answerInput = modals.find("#answerInput");
-    answerInput.val(defualtAnswer);
-
-    button = modals.find("#answerOnQuestion");
-    button.click(()=>{
-        callbackAnswered(answerInput.val());
-        modals.modal('hide');
-        button.off("click");
-    });
-
-    modals.modal("show");
-
-}
-  
-
 $(document).ready(function() {
     // reset svg each time 
     $("#svg1").attr("height", "0");
